f8c0521dd43a459c4320c578ad894b42874f5b92
manish kumar <manishkr061210@gmail.com>
2025-07-28 10:28:38 +0530

    betterAuthadded

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,51 @@
-import Image from "next/image";
+"use client";
+import { authClient } from "@/lib/auth-client";
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+import { useState } from "react";
 
 export default function Home() {
+  const {data :session} = authClient.useSession();
+  const [name ,setName] = useState("");
+  const [email ,setEmail] = useState("");
+  const [password ,setPassword] = useState("");
+  const onSubmit =()=>{
+    authClient.signUp.email({
+      email,
+      name,
+      password,
+    },{
+      onError:()=>{
+        alert("error");
+      },
+      onSuccess:()=>{
+        alert("success");
+      }
+    });
+  }
+  if(session){
+    return (
+      <div className="flex flex-col gap-y-4">
+        <h1 className="text-2xl">Welcome {session.user.name}</h1>
+        <Button onClick={()=>authClient.signOut()}>Sign Out</Button>
+      </div>
+    )
+  }
   return (
-    <div className="text-red-500 font-bold">hello</div>
+    <div>
+   <div className="flex flex-col gap-y-4">
+    <Input placeholder="name" value={name} onChange={(e)=>setName(e.target.value)}/>
+    <Input placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+    <Input placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+    <Button onClick={onSubmit}>create </Button>
+   </div>
+   
+    <div className="flex flex-col gap-y-4 mt-4">
+      <h1 className="text-2xl">Sign In</h1>
+      <Input placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+      <Input placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+      <Button onClick={() => authClient.signIn.email({ email, password })}>Sign In</Button>
+    </div>
+    </div>
   );
 }
