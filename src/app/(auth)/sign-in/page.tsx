b3753ef75a5c5028bc4e64691631571164396125
manish kumar <manishkr061210@gmail.com>
2025-08-07 09:43:24 +0530

    add signin and signup ui with logo and ui

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,6 @@
+import { SignInViews } from "@/modules/auth/ui/views/sign-in-view";
+
+const page = () => {
+    return <SignInViews/>
+}
+export default page;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import { Card } from "@/components/ui/card";
+import { SignUpViews } from "@/modules/auth/ui/views/sign-up-view";
+
+const page=()=>{
+    return<SignUpViews/>
+}
+export default page;
